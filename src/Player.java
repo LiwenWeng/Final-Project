895e980d315cca87576ad5279db33d0ec3cf3e53
ChongOscar <ohlookoscarshere@gmail.com>
2024-05-21 09:41:57 -0400

    refactored player for super class

--- src/Player.java
+++ src/Player.java
@@ -9,48 +9,56 @@ public class Player extends Entity {
     private double moveAmount;
     private String name;
     private Background background;
+    private boolean isLeftLimit;
+    private boolean isRightLimit;
 
     public Player(Background background) {
         super(100, 10, Constants.SCREEN_WIDTH * 0.5, Constants.SCREEN_HEIGHT * 0.75, true, "idle");
         this.name = "joe";
-        moveAmount = Constants.SCREEN_HEIGHT * 0.002;
+        moveAmount = Constants.SCREEN_HEIGHT * 0.0002;
         this.background = background;
+        isLeftLimit = false;
+        isRightLimit = false;
     }
 
-   public String getName() {
+    public String getName() {
        return name;
-   }
+    }
 
-   public void moveRight() {
-       background.moveRight();
-    //    if (background.isLeftLimit()) {
-    //        if (getX() + moveAmount <= Constants.SCREEN_WIDTH - getEntityImage().getWidth()) {
-    //             setX(getX() + moveAmount);
-    //        }
-    //    }
-   }
+    public void moveRight() {
+        if (background.isLeftLimit() || background.isRightLimit()) {
+            if (getX() + moveAmount <= Constants.SCREEN_WIDTH - getEntityImage().getWidth()) {
+                setX(getX() + moveAmount);
+        }
+        } else {
+            background.moveRight();
+        }
+    }
 
     public void moveLeft() {
-        background.moveLeft();
-    //    if (background.isRightLimit()) {
-    //        if (getX() - moveAmount >= 0) {
-    //            setX((getX() - moveAmount));
-    //        }
-    //    }
-   }
+        if (background.isLeftLimit() || background.isRightLimit()) {
+            if (getX() - moveAmount >= 0) {
+                setX(getX() - moveAmount);
+        }
+        } else {
+            background.moveLeft();
+        }
+    }
 
-   public void jump() {
+    public void jump() {
         if (!isGrounded()) return;
         setGrounded(false);
         setGravity(3.5);
-   }
+    }
 
-   public void simulateGravity() {
+    public void simulateGravity() {
         if (isGrounded()) return;
         setGravity(getGravity() - 0.05);
         setY(getY() - getGravity());
         if (getY() >= Constants.SCREEN_HEIGHT * 0.75) {
             setGrounded(true);
         }
-   }
+    }
+
+
 }
