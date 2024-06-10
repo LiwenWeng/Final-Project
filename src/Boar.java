64b8045ddd85c8d88f0d0adb0943751f846a2197
BT_1N3_01 <sample@bths.edu>
2024-06-10 10:58:02 -0400

    placeholder constructor for enemies

--- src/Boar.java
+++ src/Boar.java
@@ -1,5 +1,5 @@
 public class Boar extends Enemy {
-    public Boar() {
-
+    public Boar(double x, double y, Player player, Background background) {
+        super(100, 10, x, y, 100, 100, player, background);
     }
 }
