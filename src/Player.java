4b7152f0118374733a0c8d032a49c417b20262cb
apollycn <apollycn@gmail.com>
2024-06-11 18:47:47 -0400

    revert5

--- src/Player.java
+++ src/Player.java
@@ -22,7 +22,7 @@ public class Player extends Entity implements ActionListener{
     private double dashPosition;
 
     public Player(Background background, Map<String, Animation> animations) {
-        super(100, 10, 250, 800, true, animations);
+        super(100, 10, 1100, 740, true, animations);
         this.name = "joe";
 
         moveAmount = Constants.SCREEN_HEIGHT * 0.004;
