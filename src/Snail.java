e6eb997510eda0cffa8ac34439bba7ac85f768ae
apollycn <apollycn@gmail.com>
2024-06-10 23:23:41 -0400

    increased snail range

--- src/Snail.java
+++ src/Snail.java
@@ -2,6 +2,6 @@ import java.util.Map;
 
 public class Snail extends Enemy {
     public Snail(double x, double y, Player player, Background background, Map<String, Animation> animations) {
-        super(100, 10, x, y, 100, 100, player, background, animations);
+        super(100, 10, x, y, 200, 200, player, background, animations);
     }
 }
